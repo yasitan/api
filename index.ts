15b2c910c44f076bcb9264d7e7b7e0abea51eaec
Hai Le <haile.vnm@gmail.com>
2023-08-23 14:56:44 +0700

    chore(eslint): update eslint effect files

--- index.ts
+++ index.ts
@@ -12,7 +12,8 @@ import LocalUserModel from './src/db/local-user';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const DEFAULT_PORT = 3000;
+const port = process.env.PORT || DEFAULT_PORT;
 
 // connect mongodb
 connect(getEnv('APP_MONGODB_URI')).catch(err => {
@@ -23,7 +24,7 @@ connect(getEnv('APP_MONGODB_URI')).catch(err => {
 // Configure passport-local to use local user model for authentication
 passport.use(new LocalStrategy({ session: false, usernameField: 'email' }, LocalUserModel.authenticate()));
 
-passport.serializeUser(LocalUserModel.serializeUser() as any);
+passport.serializeUser(LocalUserModel.serializeUser() as never);
 passport.deserializeUser(LocalUserModel.deserializeUser());
 
 app.use(cors());
@@ -33,7 +34,7 @@ app.get('/ver', (req: Request, res: Response) => {
   res.json({ build: process.env.BUILD_NUMBER, at: process.env.BUILD_DATE, version: process.env.BUILD_VERSION });
 });
 
-app.use('/', appRouter)
+app.use('/', appRouter);
 
 app.use((err: Error & { status?: number }, request: Request, res: Response, next: NextFunction) => {
   logger.error(err, `🔴 Exception error: ${request.originalUrl}`);
