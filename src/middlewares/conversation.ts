e5ca9a3735922297f1cffcb45a6ad08ba70e00b8
Hai Le <haile.vnm@gmail.com>
2023-08-27 10:26:57 +0700

    feat: support create conversation/chat message

--- src/middlewares/conversation.ts
+++ src/middlewares/conversation.ts
@@ -4,6 +4,18 @@ import { getUserId } from './utils/request';
 import * as Conversation from '../db/conversation';
 import * as Message from '../db/message';
 import AppError from '../helpers/error';
+import uuid from '../helpers/uuid';
+
+export const createConversation = async (req: Request, res: Response, next: NextFunction) => {
+  // Default value, we will update it base on the first message from this conversation later
+  const CODE_LENGTH = 5;
+  const title = `Convo #${uuid().slice(0, CODE_LENGTH)}`;
+
+  const conversation = await Conversation.createConversation({ title, userId: getUserId(req) });
+
+  attachResponseData(res, { conversation });
+  next();
+};
 
 export const getConversations = async (req: Request, res: Response, next: NextFunction) => {
   const conversations = await Conversation.getConversations({ userId: getUserId(req) });
@@ -21,5 +33,21 @@ export const getConvoMessages = async (req: Request, res: Response, next: NextFu
   const messages = await Message.getHistoryMessages({ convoId: id });
 
   attachResponseData(res, { messages });
+  next();
+};
+
+export const chat = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  const conversation = await Conversation.getConversation({ _id: id, userId: getUserId(req) });
+  if (!conversation) {
+    return next(AppError.notFound('Conversation is not found'));
+  }
+
+  const message = await Message.createMessage({ ownerId: getUserId(req), content, convoId: id });
+
+  // TODO: ask for gpt
+
+  attachResponseData(res, { message });
   next();
 };
--- src/routes/conversation.ts
+++ src/routes/conversation.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { getConversations, getConvoMessages } from '../middlewares/conversation';
+import { chat, createConversation, getConversations, getConvoMessages } from '../middlewares/conversation';
 
 const router = Router();
 
 router.get('/', getConversations);
 router.get('/:id/messages', getConvoMessages);
+router.post('/', createConversation);
+
+router.post('/:id/chat', chat);
 
 export default router;
